d0d1a53e1890ccc9116a0af347414fc8d9997d58
Duilany <samueldps12@gmail.com>
2025-11-13 00:56:31 +0700

    Update dicodingForumApi.js

--- src/api/dicodingForumApi.js
+++ src/api/dicodingForumApi.js
@@ -1,19 +1,20 @@
-import axios from 'axios';
-import { loadToken } from '../utils/authStorage';
+import axios from "axios";
+import { loadToken } from "../utils/authStorage";
 
-const API_BASE = '/api';
+const API_BASE =
+  import.meta.env.MODE === "development"
+    ? "/api"
+    : "https://forum-api.dicoding.dev/v1";
 
 const api = axios.create({
   baseURL: API_BASE,
-  headers: { 'Content-Type': 'application/json' },
+  headers: { "Content-Type": "application/json" },
 });
 
-// attach token on each request *except public endpoints*
 api.interceptors.request.use((config) => {
   const token = loadToken();
-  const publicEndpoints = ['/leaderboards', '/threads', '/users'];
+  const publicEndpoints = ["/leaderboards", "/threads", "/users"];
 
-  // Jika endpoint publik, jangan tambahkan Authorization header
   if (!publicEndpoints.some((ep) => config.url.startsWith(ep)) && token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
